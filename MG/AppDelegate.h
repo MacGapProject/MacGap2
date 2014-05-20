0ef15fbc907eb1e8a98d55bab72b9e1d8430f14d
T. Debo <tim@rawcreativestudios.com>
2014-05-19 23:20:26 -0400

    Initial Commit

---
+++ MG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MG
+//
+//  Created by Tim Debo on 5/19/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MG/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MG
+//
+//  Created by Tim Debo on 5/19/14.
+//
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
